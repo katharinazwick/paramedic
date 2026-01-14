d4c82da438146cfc849704f7367ce1eda45d2072
katharinazwick <katharinazwick.71@gmail.com>
2026-01-15 00:55:39 +0100

    measureSelect

---
+++ src/selects/measureOption.js
@@ -0,0 +1,25 @@
+import {measure} from "../enum/measure.js";
+
+const QUERY_FIELDS = Object.values(measure).map(value => ({
+    key: value,
+    label: value,
+}));
+
+export function generateMeasureSelect(selectEl) {
+    selectEl.innerHTML = "";
+
+    // Platzhalter
+    const placeholder = document.createElement("option");
+    placeholder.value = "";
+    placeholder.textContent = "Maßnahme einreichen";
+    placeholder.disabled = true;
+    placeholder.selected = true;
+    selectEl.appendChild(placeholder);
+
+    QUERY_FIELDS.forEach(field => {
+        const option = document.createElement("option");
+        option.value = field.key;
+        option.textContent = field.label;
+        selectEl.appendChild(option);
+    });
+}
